61c779e6f6065d25fa1f58885afedd62e5459d55
OmarMHawash <omarmhawash@gmail.com>
2023-03-02 22:28:21 +0200

    changed db_name location

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const DB_NAME = 'poke_api'
+
 mongoose
   .connect(process.env.MONGODB_HOST, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log(`connected to database ${process.env.DB_NAME}`);
+    console.log(`connected to database ${DB_NAME}`);
   })
   .catch((err) => console.log("ERROR: Cannot connect to database.\n", err));
